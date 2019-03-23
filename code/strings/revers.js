9de571e7e381f6f1d887d0397ab83308c56e5a8a
mhkz <one778@163.com>
2019-03-23 17:41:37 +0800

    单词反转

---
+++ code/strings/revers.js
@@ -0,0 +1,6 @@
+/**
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ * 输入: "Let's take LeetCode contest"
+ * 输出: "s'teL ekat edoCteeL tsetnoc" 
+ */
+export default str => str.split(' ').map(item => item.split('').reverse().join('')).join(' ');
---
+++ test/strings/revers.test.js
@@ -0,0 +1,5 @@
+import revers from '../../code/strings/revers'
+
+test('revers:Let\'s take LeetCode contest', () => {
+    expect(revers("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+  })
